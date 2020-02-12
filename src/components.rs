e6876e90f2908026207147a294086e18b817fed4
Awpteamoose <awpteamoose@gmail.com>
2020-02-12 14:49:15 +0000

    submodule builder to avoid shadowing of Option

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+pub use builder::*;
 
 macro_rules! declare_basic_shortcuts {
 	($($name:ident => $element:ident),+$(,)*) => {$(
@@ -22,89 +23,93 @@ pub fn build<'a>() -> Builder<'a> {
 	Builder::default()
 }
 
-pub enum BuilderChild<'a> {
-	Owned(Box<dyn crate::Element>),
-	Ref(&'a dyn crate::Element),
-}
+pub mod builder {
+	use crate::prelude::*;
 
-#[derive(Default)]
-pub struct Builder<'a> {
-	pub text: std::option::Option<&'a str>,
-	pub attributes: std::option::Option<Vec<[&'a str; 2]>>,
-	pub class: std::option::Option<std::borrow::Cow<'a, crate::css::Style>>,
-	pub children: Vec<BuilderChild<'a>>,
-}
-
-impl<'a> Builder<'a> {
-	pub fn text(mut self, x: &'a str) -> Self {
-		self.text = Some(x);
-		self
+	pub enum BuilderChild<'a> {
+		Owned(Box<dyn crate::Element>),
+		Ref(&'a dyn crate::Element),
 	}
 
-	pub fn class(mut self, x: impl Into<std::borrow::Cow<'a, crate::css::Style>>) -> Self {
-		self.class = Some(x.into());
-		self
+	#[derive(Default)]
+	pub struct Builder<'a> {
+		pub text: Option<&'a str>,
+		pub attributes: Option<Vec<[&'a str; 2]>>,
+		pub class: Option<std::borrow::Cow<'a, crate::css::Style>>,
+		pub children: Vec<BuilderChild<'a>>,
 	}
 
-	pub fn attr(mut self, key: &'a str, value: &'a str) -> Self {
-		let mut v = self.attributes.unwrap_or_else(Vec::new);
-		v.push([key, value]);
-		self.attributes = Some(v);
-		self
-	}
-
-	pub fn child(mut self, child: impl crate::Element + 'static) -> Self {
-		self.children.push(BuilderChild::Owned(Box::new(child)));
-		self
-	}
-
-	pub fn child_ref(mut self, child: &'a (impl crate::Element + 'static)) -> Self {
-		self.children.push(BuilderChild::Ref(child));
-		self
-	}
-
-	pub fn build<T: AsRef<web_sys::Element> + AsRef<web_sys::HtmlElement> + 'static>(self, element: T) -> crate::BasicElement<T> {
-		let html_element: &web_sys::HtmlElement = element.as_ref();
-		if let Some(x) = self.text { html_element.set_inner_text(x) };
-		if let Some(x) = self.attributes {
-			for [k, v] in x {
-				html_element.set_attribute(k, v).unwrap();
-			}
-		};
-		for child in &self.children {
-			html_element.append_child(match child {
-				BuilderChild::Owned(x) => x.element(),
-				BuilderChild::Ref(x) => x.element(),
-			}).expect("Can't append child");
+	impl<'a> Builder<'a> {
+		pub fn text(mut self, x: &'a str) -> Self {
+			self.text = Some(x);
+			self
 		}
-		let cmp = crate::BasicElement {
-			element,
-			children: self.children.into_iter().filter_map(|c| if let BuilderChild::Owned(x) = c { Some(x) } else { None }).collect::<Vec<_>>(),
-			event_handlers: crate::EventHandlers::default(),
-		};
-		if let Some(x) = self.class { cmp.set_class(x); };
-		cmp
-	}
 
-	pub fn build_svg<T: AsRef<web_sys::Element> + AsRef<web_sys::SvgElement> + 'static>(self, element: T) -> crate::BasicElement<T> {
-		let svg_element: &web_sys::SvgElement = element.as_ref();
-		if let Some(x) = self.attributes {
-			for [k, v] in x {
-				svg_element.set_attribute(k, v).unwrap();
-			}
-		};
-		for child in &self.children {
-			svg_element.append_child(match child {
-				BuilderChild::Owned(x) => x.element(),
-				BuilderChild::Ref(x) => x.element(),
-			}).expect("Can't append child");
+		pub fn class(mut self, x: impl Into<std::borrow::Cow<'a, crate::css::Style>>) -> Self {
+			self.class = Some(x.into());
+			self
 		}
-		let cmp = crate::BasicElement {
-			element,
-			children: self.children.into_iter().filter_map(|c| if let BuilderChild::Owned(x) = c { Some(x) } else { None }).collect::<Vec<_>>(),
-			event_handlers: crate::EventHandlers::default(),
-		};
-		if let Some(x) = self.class { cmp.set_class(x); };
-		cmp
+
+		pub fn attr(mut self, key: &'a str, value: &'a str) -> Self {
+			let mut v = self.attributes.unwrap_or_else(Vec::new);
+			v.push([key, value]);
+			self.attributes = Some(v);
+			self
+		}
+
+		pub fn child(mut self, child: impl crate::Element + 'static) -> Self {
+			self.children.push(BuilderChild::Owned(Box::new(child)));
+			self
+		}
+
+		pub fn child_ref(mut self, child: &'a (impl crate::Element + 'static)) -> Self {
+			self.children.push(BuilderChild::Ref(child));
+			self
+		}
+
+		pub fn build<T: AsRef<web_sys::Element> + AsRef<web_sys::HtmlElement> + 'static>(self, element: T) -> crate::BasicElement<T> {
+			let html_element: &web_sys::HtmlElement = element.as_ref();
+			if let Some(x) = self.text { html_element.set_inner_text(x) };
+			if let Some(x) = self.attributes {
+				for [k, v] in x {
+					html_element.set_attribute(k, v).unwrap();
+				}
+			};
+			for child in &self.children {
+				html_element.append_child(match child {
+					BuilderChild::Owned(x) => x.element(),
+					BuilderChild::Ref(x) => x.element(),
+				}).expect("Can't append child");
+			}
+			let cmp = crate::BasicElement {
+				element,
+				children: self.children.into_iter().filter_map(|c| if let BuilderChild::Owned(x) = c { Some(x) } else { None }).collect::<Vec<_>>(),
+				event_handlers: crate::EventHandlers::default(),
+			};
+			if let Some(x) = self.class { cmp.set_class(x); };
+			cmp
+		}
+
+		pub fn build_svg<T: AsRef<web_sys::Element> + AsRef<web_sys::SvgElement> + 'static>(self, element: T) -> crate::BasicElement<T> {
+			let svg_element: &web_sys::SvgElement = element.as_ref();
+			if let Some(x) = self.attributes {
+				for [k, v] in x {
+					svg_element.set_attribute(k, v).unwrap();
+				}
+			};
+			for child in &self.children {
+				svg_element.append_child(match child {
+					BuilderChild::Owned(x) => x.element(),
+					BuilderChild::Ref(x) => x.element(),
+				}).expect("Can't append child");
+			}
+			let cmp = crate::BasicElement {
+				element,
+				children: self.children.into_iter().filter_map(|c| if let BuilderChild::Owned(x) = c { Some(x) } else { None }).collect::<Vec<_>>(),
+				event_handlers: crate::EventHandlers::default(),
+			};
+			if let Some(x) = self.class { cmp.set_class(x); };
+			cmp
+		}
 	}
 }
