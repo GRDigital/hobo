58ebef808603986e18ba57e7bc91d43aaea36640
Awpteamoose <awpteamoose@gmail.com>
2020-02-12 14:49:02 +0000

    relaxed trait bound a little bit

--- src/basic_element.rs
+++ src/basic_element.rs
@@ -1,27 +1,27 @@
 use super::{Element, EventHandler, EventHandlers, EventTarget};
 
-pub struct BasicElement<T: AsRef<web_sys::Element> + 'static> {
+pub struct BasicElement<T: AsRef<web_sys::Element>> {
 	pub element: T,
 	pub children: Vec<Box<dyn Element>>,
 	pub event_handlers: EventHandlers,
 }
 
-impl<T: AsRef<web_sys::Element> + 'static> AsRef<web_sys::Element> for BasicElement<T> {
+impl<T: AsRef<web_sys::Element>> AsRef<web_sys::Element> for BasicElement<T> {
 	fn as_ref(&self) -> &web_sys::Element { self.element.as_ref() }
 }
 
-impl<T: AsRef<web_sys::Element> + 'static> EventTarget for BasicElement<T> {
+impl<T: AsRef<web_sys::Element>> EventTarget for BasicElement<T> {
 	fn event_handlers(&self) -> std::cell::RefMut<Vec<EventHandler>> { self.event_handlers.borrow_mut() }
 }
 
-impl<T: AsRef<web_sys::Element> + 'static> BasicElement<T> {
+impl<T: AsRef<web_sys::Element>> BasicElement<T> {
 	pub fn attach_child(&mut self, child: impl Element + 'static) {
 		self.append(&child);
 		self.children.push(Box::new(child));
 	}
 }
 
-impl<T: AsRef<web_sys::Node> + AsRef<web_sys::Element> + wasm_bindgen::JsCast + 'static> BasicElement<T> {
+impl<T: AsRef<web_sys::Node> + AsRef<web_sys::Element> + wasm_bindgen::JsCast> BasicElement<T> {
 	pub fn clone_html(&self) -> Self {
 		use wasm_bindgen::JsCast;
 
@@ -30,10 +30,10 @@ impl<T: AsRef<web_sys::Node> + AsRef<web_sys::Element> + wasm_bindgen::JsCast + 
 	}
 }
 
-impl<T: AsRef<web_sys::Element> + 'static> Drop for BasicElement<T> {
+impl<T: AsRef<web_sys::Element>> Drop for BasicElement<T> {
 	fn drop(&mut self) { self.element.as_ref().remove(); }
 }
 
-impl<T: AsRef<web_sys::Element> + 'static> Element for BasicElement<T> {
+impl<T: AsRef<web_sys::Element>> Element for BasicElement<T> {
 	fn element(&self) -> &web_sys::Element { &self.element.as_ref() }
 }
