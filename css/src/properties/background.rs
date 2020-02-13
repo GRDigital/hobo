42ab7620f6094f0cbaf22f0fc19342048d9de893
Awpteamoose <awpteamoose@gmail.com>
2020-02-13 12:48:23 +0000

    non-macro background-image

--- css/src/properties/background.rs
+++ css/src/properties/background.rs
@@ -1,9 +1,31 @@
 css_macros::easy_enum! {background-repeat repeat repeat-x repeat-y no-repeat}
 css_macros::easy_enum! {background-attachment scroll fixed local}
-css_macros::easy_enum! {background-image none [raw]} // TODO:
 css_macros::easy_enum! {background-size auto cover contain @}
 css_macros::easy_enum! {background-origin border-box padding-box content-box}
 css_macros::easy_color! {background-color}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum BackgroundImage {
+	None,
+	Initial,
+	Inherit,
+	Unset,
+	Revert,
+	Some(Vec<crate::Image>),
+}
+
+impl ToString for BackgroundImage {
+	fn to_string(&self) -> String {
+		match self {
+			Self::None    => "background-image:none;".to_owned(),
+			Self::Initial => "background-image:initial;".to_owned(),
+			Self::Inherit => "background-image:inherit;".to_owned(),
+			Self::Unset   => "background-image:unset;".to_owned(),
+			Self::Revert  => "background-image:revert;".to_owned(),
+			Self::Some(x) => format!("background-image:{};", x.iter().map(std::string::ToString::to_string).collect::<Vec<_>>().join(",")),
+		}
+	}
+}
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum BackgroundPosition {
