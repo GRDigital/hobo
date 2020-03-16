82be5194b78702abc58672870465c4407d123793
Awpteamoose <awpteamoose@gmail.com>
2020-03-16 14:21:18 +0000

    can derive slot

--- hobo_derive/src/lib.rs
+++ hobo_derive/src/lib.rs
@@ -2,21 +2,15 @@ extern crate proc_macro;
 use proc_quote::quote;
 use syn::DeriveInput;
 
-#[proc_macro_derive(Element)]
+#[proc_macro_derive(Slot)]
 pub fn derive_element(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	let input = syn::parse_macro_input!(input as DeriveInput);
 	let name = input.ident;
 	let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
 	proc_macro::TokenStream::from(quote! {
-		impl #impl_generics Drop for #name #ty_generics #where_clause {
-			fn drop(&mut self) {
-				self.element().remove();
-			}
-		}
-
 		impl #impl_generics ::hobo::Element for #name #ty_generics #where_clause {
-			fn element(&self) -> ::std::borrow::Cow<'_, ::hobo::web_sys::Element> { ::std::borrow::Cow::Borrowed(&self.element) }
+			fn element(&self) -> ::std::borrow::Cow<'_, ::hobo::web_sys::Element> { self.slot.element() }
 		}
 	})
 }
@@ -41,19 +35,6 @@ pub fn derive_component(input: proc_macro::TokenStream) -> proc_macro::TokenStre
 			pub fn attach_child(&mut self, child: impl ::hobo::Element + 'static) {
 				self.element.attach_child(child)
 			}
-		}
-	})
-}
-
-#[proc_macro_derive(EventTarget)]
-pub fn derive_event_target(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let input = syn::parse_macro_input!(input as DeriveInput);
-	let name = input.ident;
-	let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
-
-	proc_macro::TokenStream::from(quote! {
-		impl #impl_generics ::hobo::EventTarget for #name #ty_generics #where_clause {
-			fn event_handlers(&self) -> ::std::cell::RefMut<Vec<::hobo::EventHandler>> { self.event_handlers.borrow_mut() }
 		}
 	})
 }
