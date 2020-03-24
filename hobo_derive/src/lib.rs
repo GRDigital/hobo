edd23b5a690c6b92864685c8ea7bcc6f39b46d39
Awpteamoose <awpteamoose@gmail.com>
2020-03-24 19:30:12 +0000

    fix derive macro

--- hobo_derive/src/lib.rs
+++ hobo_derive/src/lib.rs
@@ -1,10 +1,8 @@
-extern crate proc_macro;
 use proc_quote::quote;
-use syn::DeriveInput;
 
 #[proc_macro_derive(Slot)]
 pub fn derive_element(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let input = syn::parse_macro_input!(input as DeriveInput);
+	let input = syn::parse_macro_input!(input as syn::DeriveInput);
 	let name = input.ident;
 	let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
@@ -15,26 +13,25 @@ pub fn derive_element(input: proc_macro::TokenStream) -> proc_macro::TokenStream
 	})
 }
 
-// TODO: maybe only leave this and remove Element at all? and also maybe simplify somehow with set_inner_* accessors?
 #[proc_macro_derive(Component)]
 pub fn derive_component(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let input = syn::parse_macro_input!(input as DeriveInput);
+	let input = syn::parse_macro_input!(input as syn::DeriveInput);
 	let name = input.ident;
 	let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
-	proc_macro::TokenStream::from(quote! {
+	let impls = quote! {
 		impl #impl_generics ::hobo::Element for #name #ty_generics #where_clause {
-			fn element(&self) -> ::std::borrow::Cow<'_, ::hobo::web_sys::Element> { ::std::borrow::Cow::Borrowed(&self.element.element) }
+			fn element(&self) -> ::std::borrow::Cow<'_, ::hobo::web_sys::Element> { self.element.element() }
 		}
 
 		impl #impl_generics ::hobo::EventTarget for #name #ty_generics #where_clause {
-			fn event_handlers(&self) -> ::std::cell::RefMut<Vec<::hobo::EventHandler>> { self.element.event_handlers.borrow_mut() }
+			fn event_handlers(&self) -> ::std::cell::RefMut<::std::vec::Vec<::hobo::EventHandler>> { self.element.event_handlers() }
 		}
 
 		impl #impl_generics #name #ty_generics #where_clause {
-			pub fn attach_child(&mut self, child: impl ::hobo::Element + 'static) {
-				self.element.attach_child(child)
-			}
+			pub fn attach_child(&mut self, child: impl ::hobo::Element + 'static) { self.element.attach_child(child) }
 		}
-	})
+	};
+
+	impls.into()
 }
