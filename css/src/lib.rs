8d5d25f300cad856b6ec88b98d248379c8947e4d
Awpteamoose <awpteamoose@gmail.com>
2020-06-16 12:20:39 +0100

    can do raw property with a static str

--- css/src/lib.rs
+++ css/src/lib.rs
@@ -73,6 +73,10 @@ impl AppendProperty for String {
 	fn append_property(self, decls: &mut Vec<Property>) { decls.push(Property::Raw(self)); }
 }
 
+impl AppendProperty for &'static str {
+	fn append_property(self, decls: &mut Vec<Property>) { decls.push(Property::Raw(self.into())); }
+}
+
 impl<F: FnOnce(&mut Vec<Property>)> AppendProperty for F {
 	fn append_property(self, decls: &mut Vec<Property>) { self(decls); }
 }
