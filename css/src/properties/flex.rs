3c84517cc624c08d12be149b446d397e042c1231
Awpteamoose <awpteamoose@gmail.com>
2020-06-18 15:33:58 +0100

    missing used props

--- css/src/properties/flex.rs
+++ css/src/properties/flex.rs
@@ -5,7 +5,7 @@ css_macros::easy_enum! {justify-items auto normal stretch center start end flex-
 css_macros::easy_enum! {justify-self auto normal stretch center start end flex-start flex-end self-start self-end left right}
 css_macros::easy_enum! {align-items normal stretch center start end flex-start flex-end}
 css_macros::easy_enum! {align-content center start end flex-start flex-end normal space-between space-around space-evenly stretch}
-css_macros::easy_enum! {align-self auto normal center start end self-start self-end flex-start flex-end}
+css_macros::easy_enum! {align-self auto normal center start end self-start self-end flex-start flex-end baseline stretch}
 css_macros::easy_enum! {flex-grow [float]}
 css_macros::easy_enum! {flex-shrink [float]}
 css_macros::easy_enum! {order #}
