a82474bee6cd74ccde33ac421077e448078d39d6
Awpteamoose <awpteamoose@gmail.com>
2020-06-24 12:23:06 +0100

    RefCell<T> also replaceable

--- src/replaceable.rs
+++ src/replaceable.rs
@@ -5,10 +5,16 @@ pub trait Replaceable<T>: Element {
 	fn replace_element(&self, element: T) where Self: Sized;
 }
 
-impl<T: Element> Replaceable<T> for Rc<RefCell<T>> {
+impl<T: Element> Replaceable<T> for RefCell<T> {
 	fn replace_element(&self, element: T) {
 		let mut me = self.borrow_mut();
 		me.element().insert_adjacent_element(web_str::afterend(), &element.element()).unwrap();
 		*me = element;
+	}
+}
+
+impl<T: Element, R: Replaceable<T>> Replaceable<T> for Rc<R> {
+	fn replace_element(&self, element: T) {
+		R::replace_element(&self, element);
 	}
 }
