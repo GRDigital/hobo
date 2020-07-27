a6b878618d49bbf8fefdd92c2756e356c8973742
Awpteamoose <awpteamoose@gmail.com>
2020-07-27 16:32:24 +0100

    fix css_crate_name

--- css/css_macros/src/prelude.rs
+++ css/css_macros/src/prelude.rs
@@ -14,7 +14,7 @@ pub use proc_macro_hack::proc_macro_hack;
 pub fn css_crate_name() -> TokenStream {
 	let into_ident = |x: String| syn::Ident::new(&x, Span::call_site());
 	let hobo = proc_macro_crate::crate_name("hobo").ok().map(into_ident);
-	let hobo_css = proc_macro_crate::crate_name("hobo-css").ok().map(into_ident);
+	let hobo_css = proc_macro_crate::crate_name("hobo_css").ok().map(into_ident);
 	match (hobo, hobo_css) {
 		(Some(hobo), _) => quote! { #hobo::css },
 		(_, Some(hobo_css)) => quote! { #hobo_css },
