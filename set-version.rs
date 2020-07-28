59941ffd7333c4ecdd07c843ec882784cdbbc199
Awpteamoose <awpteamoose@gmail.com>
2020-07-28 17:16:51 +0100

    missed package version setting

--- set-version.rs
+++ set-version.rs
@@ -7,6 +7,7 @@ fn main() {
 	for manifest in &["lib/Cargo.toml", "derive/Cargo.toml", "css/Cargo.toml", "css/macros/Cargo.toml", "css/macros_decl/Cargo.toml"] {
 		let mut x = std::fs::read_to_string(manifest).unwrap();
 		x = Regex::new(r#"version = "[0-9\.]+""#              ).unwrap().replace(    &x, &format!(r#"version = "{}""#,               new_version) as &str).to_string();
+		x = Regex::new(r#"hobo_derive = "=[0-9\.]+""#         ).unwrap().replace_all(&x, &format!(r#"hobo_derive = "={}""#,          new_version) as &str).to_string();
 		x = Regex::new(r#"hobo_css = "=[0-9\.]+""#            ).unwrap().replace_all(&x, &format!(r#"hobo_css = "={}""#,             new_version) as &str).to_string();
 		x = Regex::new(r#"hobo_css_macros = "=[0-9\.]+""#     ).unwrap().replace_all(&x, &format!(r#"hobo_css_macros = "={}""#,      new_version) as &str).to_string();
 		x = Regex::new(r#"hobo_css_macros_decl = "=[0-9\.]+""#).unwrap().replace_all(&x, &format!(r#"hobo_css_macros_decl = "={}""#, new_version) as &str).to_string();
