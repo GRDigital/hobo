084eff9f3597b7ddfd07b4a5267b154c794a19d5
Awpteamoose <awpteamoose@gmail.com>
2020-07-28 16:25:46 +0100

    publsih individual packages with a publish script

--- publish.rs
+++ publish.rs
@@ -2,8 +2,18 @@
 
 #![feature(proc_macro_hygiene)]
 use command_macros::command as cmd;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-	assert!(cmd!(cargo release --dry-run).current_dir("derive").status().unwrap().success());
-	assert!(cmd!(cargo release --dry-run).current_dir("css/css_macros").status().unwrap().success());
+	assert!(cmd!(cargo release --no-confirm).current_dir("derive").status().unwrap().success());
+	assert!(cmd!(cargo publish).current_dir("derive").status().unwrap().success());
+	sleep(Duration::from_secs(60));
+	assert!(cmd!(cargo publish).current_dir("css/css_macros").status().unwrap().success());
+	sleep(Duration::from_secs(60));
+	assert!(cmd!(cargo publish).current_dir("css/css_macros_decl").status().unwrap().success());
+	sleep(Duration::from_secs(60));
+	assert!(cmd!(cargo publish).current_dir("css").status().unwrap().success());
+	sleep(Duration::from_secs(60));
+	assert!(cmd!(cargo publish).current_dir("lib").status().unwrap().success());
 }
