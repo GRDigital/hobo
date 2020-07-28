bae561a016789eb83722baf203f74ee93566973f
Awpteamoose <awpteamoose@gmail.com>
2020-07-28 16:50:37 +0100

    remove one useless sleep

--- publish.rs
+++ publish.rs
@@ -7,7 +7,6 @@ use std::time::Duration;
 
 fn main() {
 	assert!(cmd!(cargo publish).current_dir("derive").status().unwrap().success());
-	sleep(Duration::from_secs(60));
 	assert!(cmd!(cargo publish).current_dir("css/css_macros").status().unwrap().success());
 	sleep(Duration::from_secs(60));
 	assert!(cmd!(cargo publish).current_dir("css/css_macros_decl").status().unwrap().success());
