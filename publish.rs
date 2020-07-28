7be0529b6e96537776a8fbbed3644fcb7990f6ae
Awpteamoose <awpteamoose@gmail.com>
2020-07-28 15:45:06 +0100

    slightly rename

--- publish.rs
+++ publish.rs
@@ -1,16 +1,9 @@
 //# command-macros = { version = "*", features = ["nightly"] }
-//# anyhow = "*"
 
 #![feature(proc_macro_hygiene)]
 use command_macros::command as cmd;
 
-use std::fs::File;
-
-fn main() -> anyhow::Result<()> {
-	// let main_toml: toml::Value = toml::from_slice(&std::fs::read("Cargo.toml")?)?;
-	// assert_eq!(toml::ser::to_string(&main_toml)?, std::fs::read_to_string("Cargo.toml")?);
-	assert!(cmd!(cargo release --dry-run).current_dir("derive").status()?.success());
-	// assert!(cmd!(cargo test -p hobo_css).status()?.success());
-	// assert!(cmd!(wasm-pack test --headless --chrome --firefox).status()?.success());
-	Ok(())
+fn main() {
+	assert!(cmd!(cargo release --dry-run).current_dir("derive").status().unwrap().success());
+	assert!(cmd!(cargo release --dry-run).current_dir("css/css_macros").status().unwrap().success());
 }
