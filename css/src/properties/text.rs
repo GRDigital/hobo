1d77fad5b9fcf924c66b39f7da3c2bee70299d0b
Awpteamoose <awpteamoose@gmail.com>
2020-08-05 12:30:58 +0100

    font-family

--- css/src/properties/text.rs
+++ css/src/properties/text.rs
@@ -23,7 +23,6 @@ css_macros::easy_enum! {font-style normal italic oblique}
 css_macros::easy_enum! {font-size medium xx-small x-small small large x-large xx-large smaller larger [unit]}
 css_macros::easy_enum! {text-transform none capitalize uppercase lowercase}
 css_macros::easy_enum! {font-kerning auto normal none}
-css_macros::easy_enum! {font-family [string]}
 css_macros::easy_enum! {word-spacing normal [unit]}
 css_macros::easy_enum! {text-overflow clip ellipsis [string]}
 css_macros::easy_enum! {vertical-align baseline sub super top text-top middle bottom text-bottom [unit]}
@@ -39,6 +38,51 @@ css_macros::easy_color! {color}
 css_macros::easy_color! {text-decoration-color}
 css_macros::unit_value_macro! {text_indent TextIndent}
 css_macros::unit_value_macro! {outline_offset OutlineOffset}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum FontFamily {
+	Initial,
+	Inherit,
+	Unset,
+	Some(Vec<String>),
+}
+
+#[rustfmt::skip]
+impl std::fmt::Display for FontFamily {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::Initial => "font-family:initial;".fmt(f),
+			Self::Inherit => "font-family:inherit;".fmt(f),
+			Self::Unset   => "font-family:unset;".fmt(f),
+			Self::Some(fonts) => {
+				"font-family:".fmt(f)?;
+				if let Some((first, rest)) = fonts.split_first() {
+					write!(f, r#""{}""#, first)?;
+					for font in rest {
+						write!(f, r#","{}""#, font)?;
+					}
+				}
+				";".fmt(f)
+			},
+		}
+	}
+}
+
+#[macro_export]
+macro_rules! font_family {
+	(initial)         => {$crate::Property::FontFamily($crate::FontFamily::Initial)};
+	(inherit)         => {$crate::Property::FontFamily($crate::FontFamily::Inherit)};
+	(unset)           => {$crate::Property::FontFamily($crate::FontFamily::Unset)};
+	($($font:expr),+) => {$crate::Property::FontFamily($crate::FontFamily::Some(vec![$($font.into()),+]))};
+}
+
+#[test]
+fn font_family_values() {
+	assert_eq!(font_family!(initial).to_string(), "font-family:initial;");
+	assert_eq!(font_family!(inherit).to_string(), "font-family:inherit;");
+	assert_eq!(font_family!(unset).to_string(), "font-family:unset;");
+	assert_eq!(font_family!("Helvetica", "Arial", "sans-serif").to_string(), r#"font-family:"Helvetica","Arial","sans-serif";"#);
+}
 
 // css::font!(
 //     font "Roboto" 500 italic normal,
