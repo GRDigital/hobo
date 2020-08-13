52c97bc9f2b0d5dfbfc25679cc1d73b264a0a825
Awpteamoose <awpteamoose@gmail.com>
2020-08-13 15:28:31 +0100

    no more macros_decl

--- publish.rs
+++ publish.rs
@@ -9,8 +9,6 @@ fn main() {
 	assert!(cmd!(cargo publish).current_dir("derive").status().unwrap().success());
 	assert!(cmd!(cargo publish).current_dir("css/macros").status().unwrap().success());
 	sleep(Duration::from_secs(60));
-	assert!(cmd!(cargo publish).current_dir("css/macros_decl").status().unwrap().success());
-	sleep(Duration::from_secs(60));
 	assert!(cmd!(cargo publish).current_dir("css").status().unwrap().success());
 	sleep(Duration::from_secs(60));
 	assert!(cmd!(cargo publish).current_dir("lib").status().unwrap().success());
