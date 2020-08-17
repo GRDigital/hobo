6343abd2c44dadf9fd963f9ad9dc0a61fb796058
Awpteamoose <awpteamoose@gmail.com>
2020-08-17 15:54:39 +0100

    changed RawElement to support RefCell

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -35,10 +35,11 @@ thread_local! {
 /// Trait for hobo components with textual contents
 pub trait SetText<T>: RawElement<RawElementType = T>
 where
-	T: AsRef<web_sys::Element> + AsRef<web_sys::HtmlElement>,
+	T: AsRef<web_sys::Element> + AsRef<web_sys::HtmlElement> + Clone,
 {
 	fn set_text<'a>(&self, x: impl Into<std::borrow::Cow<'a, str>>) {
-		let html_element: &web_sys::HtmlElement = self.raw_element().as_ref();
+		let raw = self.raw_element();
+		let html_element: &web_sys::HtmlElement = raw.as_ref() as &web_sys::HtmlElement;
 		html_element.set_inner_text(&x.into());
 	}
 
@@ -54,7 +55,7 @@ where
 impl<T, E> SetText<E> for T
 where
 	T: RawElement<RawElementType = E>,
-	E: AsRef<web_sys::Element> + AsRef<web_sys::HtmlElement>,
+	E: AsRef<web_sys::Element> + AsRef<web_sys::HtmlElement> + Clone,
 {}
 
 pub trait ToClassStr {
