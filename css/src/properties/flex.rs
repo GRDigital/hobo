92fd021f9179121037d3c74af6569282f7133080
Gremious Grenald <gremious@protonmail.com>
2021-09-28 15:00:34 +0100

    Add align-items: baseline

--- css/src/properties/flex.rs
+++ css/src/properties/flex.rs
@@ -3,7 +3,7 @@ crate::macros::easy_enum! {flex-direction row row-reverse column column-reverse}
 crate::macros::easy_enum! {justify-content center start end flex-start flex-end left right normal space-between space-around space-evenly stretch}
 crate::macros::easy_enum! {justify-items auto normal stretch center start end flex-start flex-end self-start self-end left right}
 crate::macros::easy_enum! {justify-self auto normal stretch center start end flex-start flex-end self-start self-end left right}
-crate::macros::easy_enum! {align-items normal stretch center start end flex-start flex-end}
+crate::macros::easy_enum! {align-items baseline normal stretch center start end flex-start flex-end}
 crate::macros::easy_enum! {align-content center start end flex-start flex-end normal space-between space-around space-evenly stretch}
 crate::macros::easy_enum! {align-self auto normal center start end self-start self-end flex-start flex-end baseline stretch}
 crate::macros::easy_enum! {flex-grow [float]}
