ded4dea43ebf604bc8af723b9984d0304985d856
Awpteamoose <awpteamoose@gmail.com>
2021-11-08 18:37:01 +0000

    filter fix

--- css/src/properties/filter.rs
+++ css/src/properties/filter.rs
@@ -36,7 +36,7 @@ pub enum FilterFunction {
 	Blur(i32),
 	Brightness(F32),
 	Contrast(F32),
-	DropShadow(Unit, Unit, Option<u32>, Option<crate::Color>),
+	DropShadow(Unit, Unit, Unit, Option<crate::Color>),
 	Grayscale(F32),
 	HueRotate(F32),
 	Invert(F32),
@@ -50,7 +50,7 @@ impl FilterFunction {
 	pub fn blur(x: i32) -> Self { Self::Blur(x) }
 	pub fn brightness<T: AsPrimitive<f32>>(x: T) -> Self { Self::Brightness(F32::new(x.as_()).unwrap()) }
 	pub fn contrast<T: AsPrimitive<f32>>(x: T) -> Self { Self::Contrast(F32::new(x.as_()).unwrap()) }
-	pub fn drop_shadow(h_shadow: Unit, v_shadow: Unit, blur: Option<u32>, color: Option<crate::Color>) -> Self { Self::DropShadow(h_shadow, v_shadow, blur, color) }
+	pub fn drop_shadow(h_shadow: Unit, v_shadow: Unit, blur: Unit, color: Option<crate::Color>) -> Self { Self::DropShadow(h_shadow, v_shadow, blur, color) }
 	pub fn grayscale<T: AsPrimitive<f32>>(x: T) -> Self { Self::Grayscale(F32::new(x.as_()).unwrap()) }
 	pub fn hue_rotate<T: AsPrimitive<f32>>(x: T) -> Self { Self::HueRotate(F32::new(x.as_()).unwrap()) }
 	pub fn invert<T: AsPrimitive<f32>>(x: T) -> Self { Self::Invert(F32::new(x.as_()).unwrap()) }
@@ -70,7 +70,7 @@ impl std::fmt::Display for FilterFunction {
 			Self::Blur(x)                                     => write!(f, "blur({}px)", x),
 			Self::Brightness(x)                               => write!(f, "brightness({})", x),
 			Self::Contrast(x)                                 => write!(f, "contrast({})", x),
-			Self::DropShadow(h_shadow, v_shadow, blur, color) => write!(f, "drop-shadow({} {} {} {})", h_shadow, v_shadow, blur.unwrap_or(0), color.unwrap_or_else(|| crate::Color::from_hex(0x00_00_00_00))),
+			Self::DropShadow(h_shadow, v_shadow, blur, color) => write!(f, "drop-shadow({} {} {} {})", h_shadow, v_shadow, blur, color.unwrap_or_else(|| crate::Color::from_hex(0x00_00_00_00))),
 			Self::Grayscale(x)                                => write!(f, "grayscale({})", x),
 			Self::HueRotate(x)                                => write!(f, "hue-rotate({}deg)", x),
 			Self::Invert(x)                                   => write!(f, "invert({})", x),
