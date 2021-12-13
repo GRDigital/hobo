fe7f4a2c38d511f339ab4781c859a21768132df3
Awpteamoose <awpteamoose@gmail.com>
2021-12-13 15:02:01 +0000

    quick and dirty set version

--- set-version.rs
+++ set-version.rs
@@ -4,12 +4,13 @@ use regex::Regex;
 
 fn main() {
 	let new_version = std::env::args().skip(1).next().unwrap();
-	for manifest in &["lib/Cargo.toml", "derive/Cargo.toml", "css/Cargo.toml", "css/macros/Cargo.toml"] {
+	for manifest in &["Cargo.toml", "crates/hobo_derive/Cargo.toml", "crates/hobo_css/Cargo.toml", "crates/hobo_css_macros/Cargo.toml", "crates/hobo_core/Cargo.toml"] {
 		let mut x = std::fs::read_to_string(manifest).unwrap();
-		x = Regex::new(r#"version = "[0-9\.]+""#              ).unwrap().replace(    &x, &format!(r#"version = "{}""#,               new_version) as &str).to_string();
-		x = Regex::new(r#"hobo_derive = "=[0-9\.]+""#         ).unwrap().replace_all(&x, &format!(r#"hobo_derive = "={}""#,          new_version) as &str).to_string();
-		x = Regex::new(r#"hobo_css = "=[0-9\.]+""#            ).unwrap().replace_all(&x, &format!(r#"hobo_css = "={}""#,             new_version) as &str).to_string();
-		x = Regex::new(r#"hobo_css_macros = "=[0-9\.]+""#     ).unwrap().replace_all(&x, &format!(r#"hobo_css_macros = "={}""#,      new_version) as &str).to_string();
+		x = Regex::new(r#"\nversion = "[0-9\.]+""#     ).unwrap().replace(    &x, &format!("\nversion = \"{}\"",                                                       new_version) as &str).to_string();
+		x = Regex::new(r#"\nhobo_core = \{.+\}"#       ).unwrap().replace_all(&x, &format!("\nhobo_core = {{ path = \"crates/hobo_core\", version = \"={}\" }}",         new_version) as &str).to_string();
+		x = Regex::new(r#"\nhobo_derive = \{.+\}"#     ).unwrap().replace_all(&x, &format!("\nhobo_derive = {{ path = \"../hobo_derive\", version = \"={}\" }}",         new_version) as &str).to_string();
+		x = Regex::new(r#"\nhobo_css = \{.+\}"#        ).unwrap().replace_all(&x, &format!("\nhobo_css = {{ path = \"../hobo_css\", version = \"={}\" }}",               new_version) as &str).to_string();
+		x = Regex::new(r#"\nhobo_css_macros = \{.+\}"# ).unwrap().replace_all(&x, &format!("\nhobo_css_macros = {{ path = \"../hobo_css_macros\", version = \"={}\" }}", new_version) as &str).to_string();
 		std::fs::write(manifest, x).unwrap();
 	}
 }
