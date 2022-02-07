ebeee261be1c3674d1e7096c83d6dac5d1204177
Awpteamoose <awpteamoose@gmail.com>
2022-02-07 14:16:30 +0000

    easier transform functions

--- hobo_css/src/properties/transform.rs
+++ hobo_css/src/properties/transform.rs
@@ -74,6 +74,24 @@ pub enum TransformFunction {
 	Perspective(Unit),
 }
 
+impl TransformFunction {
+	pub fn matrix(a1: f32, b1: f32, a2: f32, b2: f32, a3: f32, b3: f32) -> Self { Self::Matrix(F32::new(a1).unwrap(), F32::new(b1).unwrap(), F32::new(a2).unwrap(), F32::new(b2).unwrap(), F32::new(a3).unwrap(), F32::new(b3).unwrap()) }
+	pub fn matrix_3d(a1: f32, b1: f32, c1: f32, d1: f32, a2: f32, b2: f32, c2: f32, d2: f32, a3: f32, b3: f32, c3: f32, d3: f32, a4: f32, b4: f32, c4: f32, d4: f32) -> Self { Self::Matrix3d(F32::new(a1).unwrap(), F32::new(b1).unwrap(), F32::new(c1).unwrap(), F32::new(d1).unwrap(), F32::new(a2).unwrap(), F32::new(b2).unwrap(), F32::new(c2).unwrap(), F32::new(d2).unwrap(), F32::new(a3).unwrap(), F32::new(b3).unwrap(), F32::new(c3).unwrap(), F32::new(d3).unwrap(), F32::new(a4).unwrap(), F32::new(b4).unwrap(), F32::new(c4).unwrap(), F32::new(d4).unwrap()) }
+	pub fn translate_x(x: Unit) -> Self { Self::TranslateX(x) }
+	pub fn translate_y(x: Unit) -> Self { Self::TranslateY(x) }
+	pub fn translate_z(x: Unit) -> Self { Self::TranslateZ(x) }
+	pub fn scale_x(x: f32) -> Self { Self::ScaleX(F32::new(x).unwrap()) }
+	pub fn scale_y(x: f32) -> Self { Self::ScaleY(F32::new(x).unwrap()) }
+	pub fn scale_z(x: f32) -> Self { Self::ScaleZ(F32::new(x).unwrap()) }
+	pub fn rotate_x(x: f32) -> Self { Self::RotateX(F32::new(x).unwrap()) }
+	pub fn rotate_y(x: f32) -> Self { Self::RotateY(F32::new(x).unwrap()) }
+	pub fn rotate_z(x: f32) -> Self { Self::RotateZ(F32::new(x).unwrap()) }
+	pub fn skew_x(x: f32) -> Self { Self::SkewX(F32::new(x).unwrap()) }
+	pub fn skew_y(x: f32) -> Self { Self::SkewY(F32::new(x).unwrap()) }
+	pub fn skew_z(x: f32) -> Self { Self::SkewZ(F32::new(x).unwrap()) }
+	pub fn perspective(x: Unit) -> Self { Self::Perspective(x) }
+}
+
 impl crate::AppendProperty for TransformFunction {
 	fn append_property(self, properties: &mut Vec<crate::Property>) {
 		Transform::Some(vec![self]).append_property(properties)
