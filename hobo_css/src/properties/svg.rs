ca2a0e0a6f41e9041c9dced256a25c69638a17cc
Gremious Grenald <gremious@protonmail.com>
2022-02-15 17:27:59 +0000

    Add stroke linecap

--- hobo_css/src/properties/svg.rs
+++ hobo_css/src/properties/svg.rs
@@ -2,6 +2,7 @@ crate::macros::easy_enum! {vector-effect none non-scaling-stroke non-scaling-siz
 crate::macros::easy_enum! {dominant-baseline auto text-bottom alphabetic ideographic middle central mathematical hanging text-top}
 crate::macros::easy_enum! {alignment-baseline auto baseline before-edge text-before-edge middle central after-edge text-after-edge ideographic alphabetic hanging mathematical top center bottom}
 crate::macros::easy_enum! {text-anchor start middle end}
+crate::macros::easy_enum! {stroke-linecap butt round square}
 crate::macros::easy_color! {fill}
 crate::macros::easy_color! {stroke}
 crate::macros::unit_value_macro! {stroke_width StrokeWidth}
