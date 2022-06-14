5b56bb3b899a9ffb2eb2f06b9b4f2e3508f95e26
Awpteamoose <awpteamoose@gmail.com>
2022-06-14 11:05:41 +0100

    syntax to enclose by expression rather than some clone always

--- hobo/src/enclose.rs
+++ hobo/src/enclose.rs
@@ -1,18 +1,6 @@
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __make_rc_upgrade_stmt {
-	// (%slot $expr:expr => $ident:ident) => {
-	//     let $ident = if let Some(x) = ::std::rc::Weak::upgrade(&$ident) { $crate::Slot(x) } else { return; };
-	// };
-	// (%slot $e:ident) => {
-	//     let $e = if let Some(x) = ::std::rc::Weak::upgrade(&$e) { $crate::Slot(x) } else { return; };
-	// };
-	// (%state $expr:expr => $ident:ident) => {
-	//     let $ident = if let (Some(data), Some(meta)) = (::std::rc::Weak::upgrade(&$ident.0), ::std::rc::Weak::upgrade(&$ident.1)) { $crate::state::State { data, meta } } else { return; };
-	// };
-	// (%state $e:ident) => {
-	//     let $e = if let (Some(data), Some(meta)) = (::std::rc::Weak::upgrade(&$e.0), ::std::rc::Weak::upgrade(&$e.1)) { $crate::state::State { data, meta } } else { return; };
-	// };
 	(% $expr:expr => $ident:ident) => {
 		let $ident = if let Some(x) = ::std::rc::Weak::upgrade(&$ident) { x } else { return; };
 	};
@@ -25,30 +13,18 @@ macro_rules! __make_rc_upgrade_stmt {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __make_stmt {
-	// (%slot $expr:expr => $ident:ident) => {
-	//     let $ident = ::std::rc::Rc::downgrade(&$expr.0);
-	// };
-	// (%slot $e:ident) => {
-	//     let $e = ::std::rc::Rc::downgrade(&$e.0);
-	// };
-	// (%state $expr:expr => $ident:ident) => {
-	//     let $ident = (::std::rc::Rc::downgrade(&$expr.data), ::std::rc::Rc::downgrade(&$expr.meta));
-	// };
-	// (%state $e:ident) => {
-	//     let $e = (::std::rc::Rc::downgrade(&$e.data), ::std::rc::Rc::downgrade(&$e.meta));
-	// };
 	(% $expr:expr => $ident:ident) => {
 		let $ident = ::std::rc::Rc::downgrade(&$expr);
 	};
 	(% $e:ident) => {
 		let $e = ::std::rc::Rc::downgrade(&$e);
 	};
-	// (* $e:ident) => {
-	//     let $e = $e.raw_element().clone();
-	// };
-	// (* $expr:expr => $ident:ident) => {
-	//     let $ident = $expr.raw_element().clone();
-	// };
+	(mut $ident:ident <- $expr:expr) => {
+		let mut $ident = $expr;
+	};
+	($ident:ident <- $expr:expr) => {
+		let $ident = $expr;
+	};
 	($expr:expr => mut $ident:ident) => {
 		let mut $ident = $expr.clone();
 	};
