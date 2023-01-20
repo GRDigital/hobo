b382757db1838266ae8e613a8d66fb1463a5db2c
Awpteamoose <awpteamoose@gmail.com>
2023-01-20 17:01:18 +0000

    a bunch of #[must_use]

--- hobo/src/create/html.rs
+++ hobo/src/create/html.rs
@@ -3,16 +3,16 @@ use super::*;
 
 pub trait BasicAttrs: AsElement {
 	#[inline] fn set_name<'a>(&self, x: impl Into<Cow<'a, str>>) { self.set_attr(web_str::name(), x); }
-	#[inline] fn name<'a>(self, x: impl Into<Cow<'a, str>>) -> Self where Self: Sized { self.set_name(x); self }
+	#[must_use] #[inline] fn name<'a>(self, x: impl Into<Cow<'a, str>>) -> Self where Self: Sized { self.set_name(x); self }
 
 	#[inline] fn set_id<'a>(&self, x: impl Into<Cow<'a, str>>) { self.set_attr(web_str::id(), x); }
-	#[inline] fn id<'a>(self, x: impl Into<Cow<'a, str>>) -> Self where Self: Sized { self.set_id(x); self }
+	#[must_use] #[inline] fn id<'a>(self, x: impl Into<Cow<'a, str>>) -> Self where Self: Sized { self.set_id(x); self }
 }
 
 impl<T: AsElement> BasicAttrs for T {}
 
 impl A {
-	#[inline] pub fn href<'a>(self, href: impl Into<Cow<'a, str>>) -> Self { self.set_href(href); self }
+	#[must_use] #[inline] pub fn href<'a>(self, href: impl Into<Cow<'a, str>>) -> Self { self.set_href(href); self }
 	#[inline] pub fn set_href<'a>(self, href: impl Into<Cow<'a, str>>) { self.set_attr(web_str::href(), href); }
 }
 
@@ -24,43 +24,43 @@ impl Input {
 		Some(vec)
 	}
 
-	#[inline] pub fn type_text(self) -> Self { self.set_type_text(); self }
+	#[must_use] #[inline] pub fn type_text(self) -> Self { self.set_type_text(); self }
 	#[inline] pub fn set_type_text(&self) { self.set_attr(web_str::r#type(), web_str::text()); }
 
-	#[inline] pub fn type_checkbox(self) -> Self { self.set_type_checkbox(); self }
+	#[must_use] #[inline] pub fn type_checkbox(self) -> Self { self.set_type_checkbox(); self }
 	#[inline] pub fn set_type_checkbox(&self) { self.set_attr(web_str::r#type(), web_str::checkbox()); }
 
-	#[inline] pub fn type_date(self) -> Self { self.set_type_date(); self }
+	#[must_use] #[inline] pub fn type_date(self) -> Self { self.set_type_date(); self }
 	#[inline] pub fn set_type_date(&self) { self.set_attr(web_str::r#type(), web_str::date()); }
 
-	#[inline] pub fn type_datetime_local(self) -> Self { self.set_type_datetime_local(); self }
+	#[must_use] #[inline] pub fn type_datetime_local(self) -> Self { self.set_type_datetime_local(); self }
 	#[inline] pub fn set_type_datetime_local(&self) { self.set_attr(web_str::r#type(), web_str::datetime_local()); }
 
-	#[inline] pub fn type_time(self) -> Self { self.set_type_time(); self }
+	#[must_use] #[inline] pub fn type_time(self) -> Self { self.set_type_time(); self }
 	#[inline] pub fn set_type_time(&self) { self.set_attr(web_str::r#type(), web_str::time()); }
 
-	#[inline] pub fn type_file(self) -> Self { self.set_type_file(); self }
+	#[must_use] #[inline] pub fn type_file(self) -> Self { self.set_type_file(); self }
 	#[inline] pub fn set_type_file(&self) { self.set_attr(web_str::r#type(), web_str::file()); }
 
-	#[inline] pub fn type_number(self) -> Self { self.set_type_number(); self }
+	#[must_use] #[inline] pub fn type_number(self) -> Self { self.set_type_number(); self }
 	#[inline] pub fn set_type_number(&self) { self.set_attr(web_str::r#type(), web_str::number()); }
 
-	#[inline] pub fn type_password(self) -> Self { self.set_type_password(); self }
+	#[must_use] #[inline] pub fn type_password(self) -> Self { self.set_type_password(); self }
 	#[inline] pub fn set_type_password(&self) { self.set_attr(web_str::r#type(), web_str::password()); }
 
-	#[inline] pub fn type_radio(self) -> Self { self.set_type_radio(); self }
+	#[must_use] #[inline] pub fn type_radio(self) -> Self { self.set_type_radio(); self }
 	#[inline] pub fn set_type_radio(&self) { self.set_attr(web_str::r#type(), web_str::radio()); }
 
-	#[inline] pub fn type_range(self) -> Self { self.set_type_range(); self }
+	#[must_use] #[inline] pub fn type_range(self) -> Self { self.set_type_range(); self }
 	#[inline] pub fn set_type_range(&self) { self.set_attr(web_str::r#type(), web_str::range()); }
 
-	#[inline] pub fn type_tel(self) -> Self { self.set_type_tel(); self }
+	#[must_use] #[inline] pub fn type_tel(self) -> Self { self.set_type_tel(); self }
 	#[inline] pub fn set_type_tel(&self) { self.set_attr(web_str::r#type(), web_str::tel()); }
 
-	#[inline] pub fn type_url(self) -> Self { self.set_type_url(); self }
+	#[must_use] #[inline] pub fn type_url(self) -> Self { self.set_type_url(); self }
 	#[inline] pub fn set_type_url(&self) { self.set_attr(web_str::r#type(), web_str::url()); }
 
-	#[inline] pub fn placeholder<'a>(self, placeholder: impl Into<Cow<'a, str>>) -> Self { self.set_placeholder(placeholder); self }
+	#[must_use] #[inline] pub fn placeholder<'a>(self, placeholder: impl Into<Cow<'a, str>>) -> Self { self.set_placeholder(placeholder); self }
 	#[inline] pub fn set_placeholder<'a>(&self, placeholder: impl Into<Cow<'a, str>>) { self.set_attr(web_str::placeholder(), placeholder); }
 
 	#[inline] pub fn get_checked(&self) -> bool { self.get_cmp::<web_sys::HtmlInputElement>().checked() }
