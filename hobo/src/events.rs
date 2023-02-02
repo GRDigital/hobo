4a29e0b79734f64f7eac58badecdcd45ba11bd7a
Awpteamoose <awpteamoose@gmail.com>
2023-02-02 14:58:43 +0000

    again rework events, it wasn't possible to subscribe to events in the middle of one firing

--- hobo/src/events.rs
+++ hobo/src/events.rs
@@ -1,21 +1,19 @@
-// use crate::prelude::*;
 use slotmap::DenseSlotMap;
 use std::{
 	any::{Any, TypeId},
 	cell::RefCell,
-	collections::{HashMap, HashSet},
-	rc::Weak,
+	collections::HashMap,
+	rc::Rc,
 };
 use once_cell::sync::Lazy;
 
 slotmap::new_key_type! {struct SubKey;}
 
-type SubFn = Box<dyn FnMut(&dyn Any)>;
+type SubFn = Rc<RefCell<dyn FnMut(&dyn Any)>>;
 
 #[derive(Default)]
 struct Events {
 	subscribers: RefCell<HashMap<TypeId, DenseSlotMap<SubKey, SubFn>>>,
-	remove_requests: RefCell<HashMap<TypeId, HashSet<SubKey>>>,
 }
 
 static EVENTS: Lazy<Events> = Lazy::new(Default::default);
@@ -34,57 +32,30 @@ unsafe impl Sync for Events {}
 // TODO: nested subscribtions etc?
 impl Events {
 	fn fire<E: Any>(&self, e: &E) {
-		let mut subscribers = self.subscribers.borrow_mut();
-		let id = TypeId::of::<E>();
-		for subscriber in subscribers.entry(id).or_default().values_mut() {
-			subscriber(e);
-		}
-		for key in self.remove_requests.borrow_mut().entry(id).or_default().drain() {
-			subscribers.get_mut(&id).unwrap().remove(key);
+		let subscribers = self.subscribers.borrow_mut().entry(TypeId::of::<E>()).or_default().values().map(Rc::clone).collect::<Vec<_>>();
+		for subcriber in subscribers {
+			subcriber.borrow_mut()(e);
 		}
 	}
 
 	fn on_key<E: Any>(&self, mut f: impl FnMut(&E) + 'static) -> SubInfo {
-		let wrapper = move |e: &dyn Any| {
-			if let Some(e) = e.downcast_ref::<E>() {
-				f(e);
-			}
-		};
 		let typeid = TypeId::of::<E>();
 		SubInfo {
-			key: self.subscribers.borrow_mut().entry(typeid).or_default().insert(Box::new(wrapper)),
+			key: self.subscribers.borrow_mut().entry(typeid).or_default().insert(Rc::new(RefCell::new(move |e: &dyn Any| f(e.downcast_ref::<E>().unwrap())))),
 			typeid,
 		}
 	}
 
 	fn on<E: Any>(&'static self, f: impl FnMut(&E) + 'static) -> Subscription {
-		Subscription(Box::new(self), self.on_key(f))
+		Subscription(self, self.on_key(f))
 	}
 
 	fn unsubscribe(&self, info: SubInfo) {
-		if let Ok(mut subscribers) = self.subscribers.try_borrow_mut() {
-			subscribers.entry(info.typeid).or_default().remove(info.key);
-		} else {
-			self.remove_requests.borrow_mut().entry(info.typeid).or_default().insert(info.key);
-		}
+		self.subscribers.borrow_mut().entry(info.typeid).or_default().remove(info.key);
 	}
 }
 
-trait Unsub {
-	fn unsubscribe(&self, info: SubInfo);
-}
-
-impl Unsub for Weak<Events> {
-	fn unsubscribe(&self, info: SubInfo) {
-		if let Some(events) = self.upgrade() { events.unsubscribe(info); }
-	}
-}
-
-impl Unsub for &'static Events {
-	fn unsubscribe(&self, info: SubInfo) { Events::unsubscribe(self, info); }
-}
-
-pub struct Subscription(Box<dyn Unsub>, SubInfo);
+pub struct Subscription(&'static Events, SubInfo);
 impl Drop for Subscription { fn drop(&mut self) { self.0.unsubscribe(self.1); } }
 
 pub fn fire<E: Any>(e: &E) { EVENTS.fire(e); }
