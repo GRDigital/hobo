097a97db12cabc3efec74dfc64ca1c1f90e2be41
Awpteamoose <awpteamoose@gmail.com>
2023-07-27 14:37:11 +0100

    syntax fix

--- publish.rs
+++ publish.rs
@@ -5,11 +5,11 @@ use std::time::Duration;
 use execute::command as cmd;
 
 fn main() {
-	assert!(cmd!(cargo publish).current_dir("hobo_derive").status().unwrap().success());
+	assert!(cmd!("cargo publish").current_dir("hobo_derive").status().unwrap().success());
 	sleep(Duration::from_secs(60));
-	assert!(cmd!(cargo publish).current_dir("hobo_css_macros").status().unwrap().success());
+	assert!(cmd!("cargo publish").current_dir("hobo_css_macros").status().unwrap().success());
 	sleep(Duration::from_secs(60));
-	assert!(cmd!(cargo publish).current_dir("hobo_css").status().unwrap().success());
+	assert!(cmd!("cargo publish").current_dir("hobo_css").status().unwrap().success());
 	sleep(Duration::from_secs(60));
-	assert!(cmd!(cargo publish).current_dir("hobo").status().unwrap().success());
+	assert!(cmd!("cargo publish").current_dir("hobo").status().unwrap().success());
 }
